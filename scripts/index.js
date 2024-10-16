ce8db576afc1f2509d72e22626b240d03a2a6e61
RomansBermans <romans.bermans@gmail.com>
2024-10-16 19:55:12 +0200

    Updates navigation

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,13 @@
 document.querySelectorAll('[data-background]').forEach(container => {
-  const url = container.getAttribute('data-background');
-  container.style.backgroundImage = `url(${url})`;
-});
+  const url = container.getAttribute('data-background')
+  container.style.backgroundImage = `url(${url})`
+})
+
+const navigation = document.getElementById('navigation')
+window.addEventListener('scroll', () => {
+  if (window.scrollY > navigation.clientHeight) {
+    navigation.classList.add('scrolled')
+  } else {
+    navigation.classList.remove('scrolled')
+  }
+})
